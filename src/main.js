c0c3d9fab4ebddcb3f2047457e1763d0a7f327b3
Jaime Aleman <jjaimealeman@gmail.com>
2022-03-08 01:31:57 -0700

    main.js // add windi-devtools, global.css, and console.log.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import 'virtual:windi.css'
+import 'virtual:windi-devtools'
+import './global.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+console.log(' %c😜 %cHA%cHA%cHA, %cmade ya look %c👀 ','font-size: 32px;','color: #ff3300; font-size: 20px','color: #00ff33; font-size: 18px','color: #3300ff; font-size: 16px; ','text-transform:uppercase; font-size: 16px; color: #ff33dd; font-style: italic;','font-size: 32px;')
