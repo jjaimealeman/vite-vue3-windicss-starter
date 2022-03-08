1e7a5164be7897eb14b5a123c95b1d82415a4382
Jaime Aleman <jjaimealeman@gmail.com>
2022-03-08 01:33:21 -0700

    HelloWorld.vue // restructure and reformat.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,38 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
-  <h1>{{ msg }}</h1>
+    <h1>{{ msg }}</h1>
+    <p>
+        Recommended IDE setup: <a href="https://code.visualstudio.com/" target="_blank">VSCode</a> + <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    </p>
+    <p>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> | <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    </p>
+    <button type="button" @click="count++"> ⬆️ </button>
+    {{ count }}
+    <button type="button" @click="count--"> ⬇️ </button>
+    <p> Edit <code>components/HelloWorld.vue</code> to test hot module replacement. </p>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <div class="text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, doloribus?</div>
 </template>
 
+
+<script setup>
+    import { ref } from 'vue';
+    defineProps({
+        msg: String
+    })
+    const count = ref(0)
+</script>
+
+
 <style scoped>
-a {
-  color: #42b983;
-}
+    /* a {
+        @apply text-green-500 hover:bg-green-100 hover:text-green-700 hover:underline
+    }
+    button {
+        @apply m-2 p-2 active:bg-transparent outline-transparent
+    }
+    code {
+        @apply bg-gray-200 rounded p-1 px-2 mx-2 border-gray-300 border-2
+    } */
+
 </style>
