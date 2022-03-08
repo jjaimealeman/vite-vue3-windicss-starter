32677ebedef9939f50cdce95d024a83fa9e72873
Jaime Aleman <jjaimealeman@gmail.com>
2022-03-08 00:15:44 -0700

    Install WindiCSS.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+import WindiCSS from 'vite-plugin-windicss'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+      WindiCSS(),
+      vue()
+    ]
 })
