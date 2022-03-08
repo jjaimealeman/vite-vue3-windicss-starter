1876755b4978ae106c7c2205c10dd1a1f1fe1945
Jaime Aleman <jjaimealeman@gmail.com>
2022-03-08 16:06:23 -0700

    App.vue // apply style to #app.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-    <img class="mx-auto py-8" alt="Vue logo" src="./assets/logo.svg" />
-    <HelloWorld msg="Hello Vue 3 + Vite" />
+    <img class="mx-auto" alt="Vue logo" src="./assets/logo.svg" />
+    <HelloWorld msg="Hello, Hey, Hi, Howdy, Hola - Vue 3 + Vite" />
 </template>
 
 
@@ -11,11 +11,14 @@
 
 <style>
 #app {
+    @apply mx-auto border-2 border-gray-100 p-4 m-4 text-center subpixel-antialiased text-gray-700;
     font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #333;
-	transition: ease all .25s;
+    /* -webkit-font-smoothing: antialiased; */
+    /* -moz-osx-font-smoothing: grayscale; */
+    /* text-align: center; */
+    /* color: #333; */
+	/* transition: ease all .25s; */
+
+
 }
 </style>
